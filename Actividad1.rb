75cfc2b0acb87284147d9ca15ec0a6a07efd36d6
Arnulfo01Vima <L20290866@cdguzman.tecnm.mx>
2024-08-29 16:58:59 +0000

    Crear archivo string

---
+++ Actividad1.rb
@@ -0,0 +1,11 @@
+class ActOne
+    def datos(nombre, edad)
+        "Hola #{nombre} tienes #{edad} años de edad"
+    end
+end
+
+
+act = ActOne.new
+puts act.datos("Arnulfo", "22")
+
+    
--- ejemplo.rb
+++ ejemplo.rb
@@ -1,16 +1,20 @@
 class Padre
+    def initialize(nombre)
+        @nombre = nombre
+    end
     def say_hello
-        saludo = "Hola desde #{self}"
-        return saludo
+        "Hola #{@nombre} desde  #{self}"
+        
     end
 end
 
 class Hijo < Padre
 end
 
-saluda1 = Padre.new
+saluda1 = Padre.new("Arnulfo")
+
 puts saluda1.say_hello
 
 
-saluda2 = Hijo.new
+saluda2 = Hijo.new("Javi")
 puts saluda2.say_hello
